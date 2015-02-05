a87e882bcc694dc44cf16038d679e891d865be60
Emil Maskovsky <emil.maskovsky@xiriar.com>
2015-02-05 21:34:31 +0100

    Add the MSVC 2010 project
    Initial empty MSVC 2010 project.

---
+++ Cookies.cpp
@@ -0,0 +1,41 @@
+/**
+    Copyright 2015 Emil Maskovsky
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+    @file
+
+    Cookies puzzle.
+
+    @author Emil Maskovsky
+
+    Determine the minimum time to reach the target with different types
+    of cookies generating credit.
+
+    Git repository: https://github.com/emaskovsky/puzzles-cookies
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    return EXIT_SUCCESS;
+}
+
+
+/* EOF */
